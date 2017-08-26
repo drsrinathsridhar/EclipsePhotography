33fb003e94fe5a20bf84b49ed2bc5e81be8489a1
Srinath Sridhar <drsrinathsridhar@gmail.com>
2017-08-26 12:23:37 -0700

    rearrange

--- makeTimeLapse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Makes a time lapse video from discrete (jpg) images
-mencoder "mf://*.jpg" -mf fps=5:type=jpg -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell:vbitrate=7000 -vf scale=640:480 -oac copy -o SunTimeLapse.avi
